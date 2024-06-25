e184eb55e4d690592176368d2dad7f130a93a45f
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-06-24 23:43:37 -0400

    idek

--- pager-app-server/index.js
+++ pager-app-server/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('sendPage', (message) => {
+    console.log('message: ', message);
+    io.emit('receievePage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
